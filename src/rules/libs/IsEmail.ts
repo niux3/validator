d60e7dfbe1988ce66cdd6f147db98ddd7d092345
niux3 <renaudbourdeau@gmail.com>
2025-02-13 02:24:33 +0100

    feat: add IsEmail

---
+++ src/rules/libs/IsEmail.ts
@@ -0,0 +1,4 @@
+export default (...args:any[])=>{
+    let pattern = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    return pattern.test(args[0])
+}
