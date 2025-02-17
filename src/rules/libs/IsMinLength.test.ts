b495222df7a9c1da8349f4dec712705209e4c86f
niux3 <renaudbourdeau@gmail.com>
2025-02-17 17:11:53 +0100

    feat: add is min length rule

---
+++ src/rules/libs/IsMinLength.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest"
+import IsMinLength from "./IsMinLength"
+
+describe("IsMinLength", () => {
+  it("should return true if the string length is greater than the required minimum as a string", () => {
+    expect(IsMinLength("abcdef", { params: "5" })).toBe(true)
+  })
+
+  it("should return false if the string length is equal to the required minimum as a string", () => {
+    expect(IsMinLength("abcde", { params: "5" })).toBe(false)
+  })
+
+  it("should return true if the string length is greater than the required minimum as a number", () => {
+    expect(IsMinLength("abcdef", { params: 5})).toBe(true)
+  })
+
+  it("should return false if the string length is equal to the required minimum as a string", () => {
+    expect(IsMinLength("abcde", { params: 5 })).toBe(false)
+  })
+})
