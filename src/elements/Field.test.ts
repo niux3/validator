f35dede0e00b354db5e3a3800f424dc57e3d96bb
niux3 <renaudbourdeau@gmail.com>
2025-02-19 12:23:18 +0100

    feat: upgrade field test

--- src/elements/Field.test.ts
+++ src/elements/Field.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, vi } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 import { Field } from './Field'
 import { ElementHTMLProperties } from './ElementHTML.type'
 import { Rules } from '../rules/Rules'
