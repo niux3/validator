863b5a797b95244d37715fa5cdc6d4affe2cf9cc
niux3 <renaudbourdeau@gmail.com>
2025-02-26 17:36:57 +0100

    doc: add tsdoc in formsGroup class

--- src/elements/FormsGroup.ts
+++ src/elements/FormsGroup.ts
@@ -4,10 +4,18 @@ import { AppConfiguration } from './FormsGroup.type'
 import { ElementHTMLProperties } from './ElementHTML.type'
 
 
+/**
+ * Represents a group of forms managed by the application.
+ * This class handles the creation, removal, and retrieval of forms.
+ */
 export class FormsGroup{
     private forms:Form[] = []
     private configuration:AppConfiguration
     
+    /**
+     * Creates an instance of FormsGroup.
+     * @param {Configuration} configuration - The configuration object containing form settings and rules.
+    */
     constructor(configuration:Configuration){
         this.configuration = configuration.get() as AppConfiguration
         
@@ -18,6 +26,10 @@ export class FormsGroup{
         }
     }
 
+    /**
+     * Adds a new form to the forms group.
+     * @param {HTMLFormElement} $form - The HTML form element to add.
+    */
     addForm($form:HTMLFormElement): void{
         let props:ElementHTMLProperties = {
             element : $form,
@@ -34,6 +46,10 @@ export class FormsGroup{
         this.forms = [...this.forms, new Form(props)]
     }
 
+    /**
+     * Removes a form from the forms group.
+     * @param {HTMLFormElement} $form - The HTML form element to remove.
+    */
     rmForm($form:HTMLFormElement): void{
         this.forms.forEach((form, i)=>{
             if(form.$el === $form){
@@ -42,6 +58,10 @@ export class FormsGroup{
         })
     }
 
+    /**
+     * Retrieves all forms in the forms group.
+     * @returns {Form[]} An array of Form instances.
+    */
     get(): Form[]{
         return this.forms
     }
