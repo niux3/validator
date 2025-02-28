ace858683a34f60d9b300ad345cb38a3c3875fe1
niux3 <renaudbourdeau@gmail.com>
2025-02-28 17:23:30 +0100

    doc: tsdoc is isdate

--- src/rules/libs/IsDate.ts
+++ src/rules/libs/IsDate.ts
@@ -1,15 +1,36 @@
-export default (...args:any[]) => {
-    if (['', null, undefined].some(e => e === args[0])) {
+/**
+ * Validates if a string is a valid date in the format `YYYY-MM-DD` within a reasonable year range (1900-2100).
+ *
+ * This function checks if the provided string:
+ * 1. Is not empty, `null`, or `undefined`.
+ * 2. Matches the format `YYYY-MM-DD`.
+ * 3. Represents a valid date (e.g., valid month, day, and year ranges).
+ *
+ * @param {string} value - The string to validate.
+ * @returns {boolean} - Returns `true` if the string is a valid date in the format `YYYY-MM-DD`, otherwise `false`.
+ *
+ * @example
+ * // Example 1: Valid date
+ * const isValid1 = isValidDate("2023-10-05");
+ * console.log(isValid1); // true
+ *
+ * @example
+ * // Example 2: Invalid date
+ * const isValid2 = isValidDate("1899-02-30");
+ * console.log(isValid2); // false
+*/
+export default (value:string) => {
+    if (['', null, undefined].some(e => e === value)) {
         return false
     }
 
-    // Vérifie si la date correspond au format YYYY-MM-DD
-    let match = args[0].match(/^(\d{4})-(\d{2})-(\d{2})$/)
+    // check if format YYYY-MM-DD
+    let match = value.match(/^(\d{4})-(\d{2})-(\d{2})$/)
     if (!match) {
-        return false // La date ne respecte pas le format attendu
+        return false
     }
 
-    let [_, y, m, d] = match.map(Number) // Convertit en nombres
+    let [_, y, m, d] = match.map(Number) // Convertit in Number
 
     return (
         m > 0 &&
