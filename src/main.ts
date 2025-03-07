29e0d1e0c915e1de3b47c7758495ff7fbe78fcb8
niux3 <renaudbourdeau@gmail.com>
2025-03-07 18:50:25 +0100

    feat: begin example

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,223 @@
 // @ts-nocheck
 import Validator from './Validator'
-import { IsUrl } from './rules/libs'
 
-let options = {
-    'selector': '.testForm',
-    'fields': {
-        'firstname': {
-            'isnotempty': {
-                'error': 'ne doit pas être vide (params)',
-                'success': '!!',
+
+let defaultErrorMessage = "Ce champ ne doit pas être vide",
+    optionValidator = {
+        "selector" : ".formToValidate",
+        "mode" : "object",
+        "fields" : {
+            "civility" : {
+                "target" : {
+                    "error" : "#messageCivility",
+                },
+                "isnotempty" : {
+                    "error" : defaultErrorMessage
+                }
             },
-            "isurl":{
-                "error": 'url'
+            "firstname" : {
+                "target" : {
+                    "error" : ".messageFirstname",
+                },
+                "isnotempty":{
+                    "error" : defaultErrorMessage,
+                },
+                "isminlength":{
+                    'params': 2,
+                    "error" : "Ce champ doit avoir minimum 3 caractères",
+                }
+            },
+            "lastname" : {
+                "target" : {
+                    "error" : ".messageLastname",
+                },
+                "isnotempty":{
+                    "error" : defaultErrorMessage
+                },
+                "isminlength":{
+                    'params': 2,
+                    "error" : "Ce champ doit avoir minimum 3 caractères"
+                }
+            },
+            "email" : {
+                "target" : {
+                    "error" : "#messageEmail",
+                },
+                "isnotempty":{
+                    "error" : defaultErrorMessage
+                },
+                "isemail":{
+                    "error" : "Ce champ doit avoir la bonne saisie (dom@dom.com)"
+                }
+            },
+            "subject" : {
+                "target" : {
+                    "error" : "#messageSubject",
+                },
+                "isnotempty":{
+                    "error" : defaultErrorMessage
+                },
+                "isminlength":{
+                    'params': 2,
+                    "error" : "Ce champ doit avoir minimum 3 caractères"
+                }
+            },
+            "message" : {
+                "target" : {
+                    "error" : "#messageMessage",
+                },
+                "isnotempty":{
+                    "error" : defaultErrorMessage
+                },
+                "isminlength":{
+                    'params': 2,
+                    "error" : "Ce champ doit avoir minimum 3 caractères"
+                }
+            },
+            "validatePhone" : {
+                "target" : {
+                    "error" : "#messagePhoneChoice",
+                },
+                "isnotempty" : {
+                    "error" : defaultErrorMessage
+                }
+            },
+            "hobbiesChoice" : {
+                "target" : {
+                    "error" : ".choiceHobbiesMessage",
+                },
+                "isnotempty" : {
+                    "error" : defaultErrorMessage
+                }
+            },
+            "phone" : {
+                "target" : {
+                    "error" : "#messagePhone",
+                },
+                "checkphone" : {
+                    "error" : "Le format ne semble pas être bon 0102030405"
+                },
+                "isnotempty" : {
+                    "error" : defaultErrorMessage
+                }
+            },
+            "hobbies[]":{
+                "target" : {
+                    "error" : ".hobbiesMessage",
+                },
+                "isminlength":{
+                    'params' : 1,
+                    "error" : "vous devez choisir au moins 2 loisirs"
+                },
+                "ismaxlength":{
+                    'params' : 4,
+                    "error" : "vous devez choisir au maximum 3 loisirs"
+                },
+                "isnotempty":{
+                    "error" : defaultErrorMessage
+                },
+            },
+            "password" : {
+                "isnotempty":{
+                    "error" : defaultErrorMessage
+                }
+            },
+            "selectMultiple[]" : {
+                "isminlength":{
+                    'params' : 1,
+                    "error" : "vous devez choisir au moins 2 items"
+                },
+                "ismaxlength":{
+                    'params' : 4,
+                    "error" : "vous devez choisir au maximum 3 items"
+                },
+                "isnotempty":{
+                    "error" : defaultErrorMessage
+                },
+            },
+            "checkpassword" : {
+                "isnotempty":{
+                    "error" : defaultErrorMessage
+                },
+                "isequalto":{
+                    "params" : '#password',
+                    "error" : "Ce champ doit être identique au champs mot de passe"
+                }
+            },
+            "dateAppointement" : {
+                "target" : {
+                    "error" : "#messageDate",
+                },
+                "isnotempty":{
+                    "error" : defaultErrorMessage
+                },
+                "isdate":{
+                    "error" : "Ce champ doit être une date valide"
+                }
             }
         }
-    }
+    },
+    validate = new Validator(optionValidator)
+
+//add rules for phone input
+validate.addRules('checkphone', (value)=>{
+    return !/^0[1-8][ .-]?(\d{2}[ .-]?){4}$/.test(value);
+})
+//check form when it's submitted
+validate.form()
+
+let listRequireField = [
+    'input[type=text]',
+    'input[type=date]',
+    'input[type=password]',
+    // 'input[type=radio]',
+    'input[type=checkbox]',
+    'select',
+    'textarea',
+]
+
+//check each element(input/select/textarea) when it's in focus and has lost focus
+document.querySelectorAll(listRequireField.join(',')).forEach(($field)=>{
+    $field.addEventListener('blur', (e)=>{
+        validate.element(e.target)
+    })
+})
+
+//add require field if you answer 'yes' at 'add phone number'
+if(document.getElementById('phone')){
+    let $phone = document.getElementById('phone')
+    $phone.parentNode.style.display = 'none'
+    document.getElementsByName('validatePhone').forEach(($input) =>{
+        $input.addEventListener('change', (e)=>{
+            if(e.target.checked && e.target.id === "validatePhoneYes"){
+                $phone.parentNode.style.display = 'block'
+                validate.addRequireField($phone)
+            }else{
+                $phone.parentNode.style.display = 'none'
+                validate.removeRequireField($phone)
+            }
+        })
+    })
 }
-let validator = new Validator(options)
-let $myForm = document.getElementById('myForm')
-let $lastname = document.getElementById('lastname')
+//add require fields if you answer 'yes'
+if(document.getElementsByName('hobbies[]')){
+    let $hobbies = document.getElementsByName('hobbies[]'),
+        $hobbiesContainer = document.getElementById('hobies-container');
+    document.getElementsByName('hobbiesChoice').forEach(($radio) =>{
+        $radio.addEventListener('change', (e)=>{
+            if(e.target.checked && e.target.id === "hobbiesYes"){
+                $hobbiesContainer.style.display = 'block';
+                $hobbies.forEach(($checkbox)=>{
+                    validate.addRequireField($checkbox);
+                });
+            }else{
+                $hobbiesContainer.style.display = 'none';
+                $hobbies.forEach(($checkbox)=>{
+                    validate.removeRequireField($checkbox);
+                });
+            }
+        });
+    });
+}
 
-//$myForm?.addEventListener('submit', e => validator.checkForm($myForm, e))
-
-validator.addRules('isimmatriculation', value => /^\d{3,4}[a-z]{2,3}\d{2}[a-z]?$/i.test(value.trim()))
-
-validator.form()
-let myForm = document.getElementById('myForm')
-validator.addRequireForm(myForm)
-validator.form()
-
-document.querySelector('.rmForm')?.addEventListener('click', e =>{
-    validator.removeRequireForm(myForm)
-    myForm.remove()
-})
-
-document.querySelector('.addField')?.addEventListener('click', e =>{
-    let tpl = document.getElementById('fieldUsername'),
-        contentTpl = tpl.content.cloneNode(true).firstElementChild
-    console.log(tpl.content.cloneNode(true).firstElementChild)
-    document.querySelector('.testForm')?.insertAdjacentElement('afterbegin', contentTpl)
-    validator.addRequireField(contentTpl.querySelector('input'))
-
-})
-
-document.querySelector('.rmField')?.addEventListener('click', e =>{
-    let username = document.querySelector('#username')
-    validator.removeRequireField(username)
-    username.closest('label').remove()
-})
-
-
-$lastname?.addEventListener('blur', e => validator.element($lastname))
-
-
-console.log('>>', validator.check('bla' , 'isurl'))
-console.log("->", IsUrl('www.renaud.com'))
+console.log("'dom@dom' ===  format email --> ", validate.check('dom@dom', 'isemail'))
